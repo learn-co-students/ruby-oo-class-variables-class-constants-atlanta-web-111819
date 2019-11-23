45a9e16265b2965d947d4d0d99db6a0541fee967
github username <github email address>
2019-11-23 14:27:00 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    self.add_brand
+    BRANDS.uniq
   end
 
   def cobble
@@ -11,4 +17,15 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def add_brand 
+    if BRANDS.include?(self.brand)
+      return BRANDS
+    else 
+      BRANDS << self.brand 
+      return BRANDS
+    end 
+  end 
+
+
+  
 end
